22a828e3a6113c15884b31ea4875bfb43e381a30
Muhammad Rafi <iamyawpie@protonmail.com>
2024-05-12 15:01:37 +0800

    WIP: 3434b84 register & login screen color update

--- public/scripts/confirm_password.js
+++ public/scripts/confirm_password.js
@@ -1,11 +1,18 @@
-const register_button = document.getElementById("register_button");
+const register_form = document.getElementById("register_form");
 
-register_button.addEventListener("click", (e) => {
+register_form.addEventListener("submit", (event) => {
   const password = document.getElementById("password");
   const password_confirm = document.getElementById("password_confirm");
+  console.log("register pressed");
 
   if (password.value !== password_confirm.value) {
-    alert("Password tidak cocok");
-    e.preventDefault();
+    // alert("Password tidak cocok");
+    event.preventDefault();
+    console.log("password mismatch");
+    console.log(password.value, " ", password_confirm.value);
+    password.setCustomValidity("Password tidak cocok");
+    password_confirm.setCustomValidity("Password tidak cocok");
+    password.reportValidity();
+    password_confirm.reportValidity();
   }
 });
---
+++ public/scripts/password_check.js
@@ -0,0 +1,3 @@
+const user = document.getElementById("email");
+
+fetch();
