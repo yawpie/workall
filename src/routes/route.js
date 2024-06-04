f32952609bc38232fd8a2aff608f42fd1e6b67f4
Holid-Kurang <kh5lilulloh@gmail.com>
2024-06-04 16:06:12 +0800

    add header login and some update

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,7 @@ const register = require("./register");
 const login = require("./login");
 const landing_page = require("./landing_page");
 const open_shop = require("./open_shop");
+const tes = require("./tes");
 
 route.use(express.urlencoded({ extended: true }));
 route.use(express.json());
@@ -16,5 +17,6 @@ route.use("/register", register);
 route.use("/login", login);
 route.use("/", landing_page);
 route.use("/open_shop", open_shop);
+route.use("/tes", tes);
 
 module.exports = route;
