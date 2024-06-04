5fd79e132166ec370e7f9c17ca206e0befd4d305
Holid-Kurang <kh5lilulloh@gmail.com>
2024-06-05 04:21:53 +0800

    add openlayanan page and update popup notif and pesanan

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,7 @@ const register = require("./register");
 const login = require("./login");
 const landing_page = require("./landing_page");
 const open_shop = require("./open_shop");
+const open_layanan = require("./open_layanan");
 const tes = require("./tes");
 
 route.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,7 @@ route.use("/register", register);
 route.use("/login", login);
 route.use("/", landing_page);
 route.use("/open_shop", open_shop);
+route.use("/open_layanan", open_layanan);
 route.use("/tes", tes);
 
 module.exports = route;
